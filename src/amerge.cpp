3c4b39b4ee2d017b166d520b3e9a025672468b1c
Christoph Peltz <kris@keeg.de>
2008-04-11 10:43:22 +0200

    Implemented scan_directory, only needs some more helpers.

--- src/amerge.cpp
+++ src/amerge.cpp
@@ -1,7 +1,10 @@
 #include "amerge.hpp"
 #include <iostream>
+#include <boost/filesystem.hpp>
 
 #define foreach BOOST_FOREACH
+
+namespace fs = boost::filesystem;
 
 using std::cout;
 using std::endl;
@@ -10,18 +13,36 @@ using std::flush;
 struct Merge{
 	int num_files;
 	int num_dirs;
-	std::vector< std::string > paths;
+	std::vector< fs::path > paths;
 	Merge():num_files(0),num_dirs(0){}
 };
 
-void scan_directory(Merge &status, std::string directory){
-	std::vector< std::string > ls = get_directory(directory);
-	foreach(std::string entry, ls){
-		if(is_file(entry) && check_ending(entry)){
+bool has_valid_extension( const fs::path &path ) {
+	std::string ending = path.leaf();
+	std::string::size_type pos = ending.find_last_of( '.' );
+	ending = ending.substr( pos + 1 );
+	ending.to_lower();
+	foreach( const char* ext, extensions ) {
+		if(endling == ext) {
+			return true;
+		}
+	}
+	return false;
+}
+
+void scan_directory(Merge &status, const fs::path &directory) { // TODO Implement exists(), is_directory(), is_file() and has_valid_extension()
+	if( !exists(directory) ) {
+		cout << "ERROR: Directory " << directory << " does not exist!" << endl;
+		return;
+	}
+	fs::directory_iterator dir_iter = directory.begin(), end_iter;
+	for( ; dir_iter != end_iter; dir_iter++ ){
+		if( is_directory(*dir_iter) ) {
+			status.num_dirs++;
+			scan_directory( *dir_iter );
+		} else if ( is_file(*dir_iter) && has_valid_extension(*dir_iter) ) {
 			status.num_files++;
-			status.paths.push_back(directory + "/" + entry);
-		} else if(is_dir(entry)) {
-			scan_directory(status, directory + "/" + entry);
+			status.paths.push_back( *dir_iter );
 		}
 	}
 }
@@ -33,6 +54,8 @@ void AMerge::perform_action_merge(){
 	foreach(std::string directory, _directories){
 		scan_directory(status, directory);
 	}
+	cout << "done" << endl;
+	cout << "Scanned " << status.num_files << " files in " << status.num_dirs << "directories" << endl;
 }
 
 void AMerge::perform_action_defrag(){}
