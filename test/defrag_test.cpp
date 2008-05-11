1cc4fb8f7ea8b83ffb65b63ae1ee85020c3c63fe
Christoph Peltz <kris@keeg.de>
2008-05-11 21:43:30 +0200

    Added two more tests and a test stub (actions are in need for tests)

---
+++ test/defrag_test.cpp
@@ -0,0 +1,32 @@
+#include "../src/common.cpp"
+#include "../src/defrag.cpp"
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( test_move ) {
+	fs::create_directory(".test");
+	fs::create_directory(".test/src");
+	fs::create_directory(".test/dest");
+	std::ofstream file( "file" );
+	BOOST_CHECK( !file.fail() );
+	file.close();
+	fs::copy_file( "file", fs::path(".test") / "src" / "file1.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "src" / "file2.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "src" / "file3.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "src" / "file4.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "src" / "file5.jpg" );
+	move( fs::path(".test/src/"), fs::path(".test/dest/") );
+	BOOST_CHECK( fs::is_empty(fs::path(".test/src")) );
+	BOOST_CHECK( !fs::is_empty(fs::path(".test/dest")) );
+	BOOST_CHECK( fs::is_regular(fs::path(".test/dest/file3.jpg")) );
+	fs::remove( fs::path(".test") / "dest" / "file1.jpg" );
+	fs::remove( fs::path(".test") / "dest" / "file2.jpg" );
+	fs::remove( fs::path(".test") / "dest" / "file3.jpg" );
+	fs::remove( fs::path(".test") / "dest" / "file4.jpg" );
+	fs::remove( fs::path(".test") / "dest" / "file5.jpg" );
+	fs::remove( fs::path(".test") / "dest" );
+	fs::remove( fs::path(".test") / "src" );
+	fs::remove( fs::path(".test") );
+	fs::remove( fs::path("file") );
+}
---
+++ test/unique_test.cpp
@@ -0,0 +1,32 @@
+#include "../src/common.cpp"
+#include "../src/unique.cpp"
+#define BOOST_AUTO_TEST_MAIN
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_CASE( test_CRCTable ) {
+	fs::create_directory(".test");
+	std::ofstream file( "file" );
+	BOOST_CHECK( !file.fail() );
+	file << "Beer";
+	file.close();
+	fs::copy_file( "file", fs::path(".test") / "file1.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "file2.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "file3.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "file4.jpg" );
+	fs::copy_file( "file", fs::path(".test") / "file5.jpg" );
+	file.open( "file" );
+	BOOST_CHECK( !file.fail() );
+	file << "Kekse";
+	file.close();
+	fs::copy_file( "file", fs::path(".test") / "file6.jpg" );
+	CRCTable table;
+	table.scan_directory( fs::path(".test") );
+	table.sort();
+	table.create_crc();
+	BOOST_CHECK( table.unique() == 4 );
+	fs::remove( fs::path(".test/file1.jpg") );
+	fs::remove( fs::path(".test/file6.jpg") );
+	fs::remove( fs::path(".test") );
+	fs::remove( fs::path("file") );
+}
